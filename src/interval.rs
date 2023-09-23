626ac455f95a01deb181383b98ba63f4db898e19
Daniel Way <projects@danieldway.com>
2023-09-23 09:03:44 -0500

    Add `Interval.clamp`

--- src/interval.rs
+++ src/interval.rs
@@ -8,11 +8,11 @@ impl Interval {
     pub fn new(min: f64, max: f64) -> Self {
         Self { min, max }
     }
-    
+
     pub fn empty() -> Self {
         Self { min: f64::INFINITY, max: f64::NEG_INFINITY }
     }
-    
+
     pub fn universe() -> Self {
         Self { min: f64::NEG_INFINITY, max: f64::INFINITY }
     }
@@ -28,9 +28,19 @@ impl Interval {
     pub fn contains(self, x: f64) -> bool {
         self.min <= x && x <= self.max
     }
-    
+
     pub fn surrounds(self, x: f64) -> bool {
         self.min < x && x < self.max
+    }
+
+    pub fn clamp(self, x: f64) -> f64 {
+        if x < self.min {
+            self.min
+        } else if x > self.max {
+            self.max
+        } else {
+            x
+        }
     }
 }
 impl Default for Interval {
