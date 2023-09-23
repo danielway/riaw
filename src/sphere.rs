b561961902bbb85ae99895c176822186828ddf21
Daniel Way <projects@danieldway.com>
2023-09-22 22:00:35 -0500

    Define hittable trait and move sphere to struct

---
+++ src/sphere.rs
@@ -0,0 +1,53 @@
+use crate::hittable::{HitRecord, Hittable};
+use crate::ray::Ray;
+use crate::vec3::Point3;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
+    }
+
+    pub fn center(self) -> Point3 {
+        self.center
+    }
+
+    pub fn radius(self) -> f64 {
+        self.radius
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: Ray, t_min: f64, t_max: f64, record: &mut HitRecord) -> bool {
+        let oc = ray.origin() - self.center();
+        let a = ray.direction().length_squared();
+        let half_b = oc.dot(ray.direction());
+        let c = oc.length_squared() - self.radius() * self.radius();
+
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return false;
+        }
+
+        let sqrtd = discriminant.sqrt();
+
+        let mut root = (-half_b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return false;
+            }
+        }
+
+        record.t = root;
+        record.point = ray.at(root);
+        record.normal = (record.point - self.center()) / self.radius();
+
+        true
+    }
+}
