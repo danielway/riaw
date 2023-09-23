26f7343e3c018bdbc84bd402a17ccc0b1fbd7382
Daniel Way <projects@danieldway.com>
2023-09-22 21:04:26 -0500

    Implement PPM writer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use crate::ppm_writer::write_image;
+
+mod ppm_writer;
+
 fn main() {
     println!("Hello, world!");
+
+    let data = vec![
+        (255, 0, 0),
+        (0, 255, 0),
+        (0, 0, 255),
+        (255, 255, 0),
+        (255, 255, 255),
+        (0, 0, 0),
+    ];
+
+    write_image("test.ppm", 2, 3, data).expect("Failed to write image");
 }
